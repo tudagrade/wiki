b2ef57caaec420644919366668abde237f990091
Alexander Staeding <alexanderstaeding@gmail.com>
2022-03-11 22:40:28 +0100

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Haudy`,
+    defaultTitle: `Haudy`,
+    siteTitleShort: `Haudy`,
+    siteDescription: ``,
+    siteUrl: `https://haudy.tudalgo.github.io`,
+    siteAuthor: 'alexstaeding',
+    siteImage: ``,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        githubUrl: `https://github.com/SourceGrade/JagrDocs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.com`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`,
+  ],
+};
---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Logo(props) {
+    return (
+        <svg width={180} height={34} viewBox="0 0 180 34" fill="none" {...props}>
+            <path
+                d={""}
+                fill={""}
+            />
+            <path
+                d={""}
+                fill={""}
+            />
+        </svg>
+    );
+}
